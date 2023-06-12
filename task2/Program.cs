9ef0fc31136ef0e5f2e47719b54100787795ec67
JarLaX99 <vanek10699@gmail.com>
2023-06-12 18:02:26 +0300

    Вторая задача

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое число:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число:");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB && numberA > numberC)
+{
+    Console.WriteLine($"первое число больше");
+}
+if (numberB > numberA && numberB > numberC)
+{
+    Console.WriteLine($" второе число больше");
+}
+if (numberC > numberA && numberC > numberB)
+{
+    Console.WriteLine("Третье число больше");
+}
+else if (numberA == numberB && numberB ==  numberC)
+{
+    Console.WriteLine($"числа  равны");
+}
